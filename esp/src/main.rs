c426452b425f58d93be650a46a5541ca20d6993d
outragedline <outragedline@proton.me>
2025-06-15 05:04:09 -0300

    mqtt on esp

--- esp/src/main.rs
+++ esp/src/main.rs
@@ -1,12 +1,16 @@
 use esp_idf_svc::sys::*;
 mod provisioning;
 
+use std::time::Duration;
+
 use embedded_svc::wifi::{ClientConfiguration, Configuration};
 use esp_idf_svc::hal::prelude::Peripherals;
+use esp_idf_svc::mqtt::client::*;
 use esp_idf_svc::wifi::{BlockingWifi, EspWifi};
 use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition};
 
-fn main() -> Result<(), EspError> {
+const MQTT_URL: &str = env!("MQTT_URL");
+fn main() -> anyhow::Result<()> {
     esp_idf_svc::sys::link_patches();
 
     let peripherals = Peripherals::take()?;
@@ -27,8 +31,8 @@ fn main() -> Result<(), EspError> {
         wifi.start()?;
         prov.start_provisioning(
             wifi_prov_security_WIFI_PROV_SECURITY_1,
-            "teleporta", // Proof of Possession (POP)
-            "PROV_TELEPORTA",      // Service Name
+            "teleporta",      // Proof of Possession (POP)
+            "PROV_TELEPORTA", // Service Name
             None,             // No Service Key
         )?;
 
@@ -45,5 +49,52 @@ fn main() -> Result<(), EspError> {
     let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
     println!("Wifi DHCP info: {:?}", ip_info);
 
-    Ok(())
+    let (mut mqtt_client, mut mqtt_conn) = EspMqttClient::new(
+        MQTT_URL,
+        &MqttClientConfiguration {
+            client_id: Some("esp_teleporta"),
+            ..Default::default()
+        },
+    )?;
+
+    std::thread::scope(|s| {
+        // This is required it seems
+        std::thread::Builder::new()
+            .stack_size(6000)
+            .spawn_scoped(s, move || {
+                println!("MQTT Listening for messages");
+
+                while let Ok(event) = mqtt_conn.next() {
+                    println!("[Queue] Event: {}", event.payload());
+                }
+            })
+            .unwrap();
+
+        loop {
+            if let Err(_) = mqtt_client.subscribe("door/alert", QoS::AtMostOnce) {
+                std::thread::sleep(Duration::from_millis(500));
+
+                continue;
+            }
+
+            println!("Subscribed to topic \"door/alert\"");
+
+            // Just to give a chance of our connection to get even the first published message
+            std::thread::sleep(Duration::from_millis(500));
+
+            loop {
+                mqtt_client
+                    .publish("door/alert", QoS::AtMostOnce, false, "".as_bytes())
+                    .unwrap();
+
+                let sleep_secs = 2;
+
+                println!("Now sleeping for {sleep_secs}s...");
+                std::thread::sleep(Duration::from_secs(sleep_secs));
+            }
+        }
+    });
+    loop {
+        std::thread::sleep(Duration::from_secs(1));
+    }
 }
